ac8434d6844bdbf92c3fbfd640908fffb3f7bd19
fluxlog <fluxlog@hotmail.com>
2020-04-28 10:03:50 -0700

    modifed register endpoint

--- api/server.spec.js
+++ api/server.spec.js
@@ -28,6 +28,21 @@ describe('server', function () {
         });
     });
 
+    it('should set db environment to testing', function() {
+      expect(process.env.DB_ENV).toBe('testing');
+    });
+
+    describe('server.js', () => {
+      describe('index route', () => {
+        it('returns an OK status code from the index route', async () => {
+          const expectedStatusCode = 200;
+    
+          const response = await request(server).get('/');
+          expect(response.status).toEqual(expectedStatusCode);
+        });
+      });
+    });
+
     it('should show server is running', function () {
       // make a GET request to /
       return request(server)
